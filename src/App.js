91ccb5f6cb3781a3d4f9be5ec71f8f4ebfbd7f3a
alisa <alisa@dxt.cn>
2018-03-29 14:29:52 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import logo from './logo.svg';
+import HomePage from './routes/homPage/homePage.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/" component={HomePage} />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
