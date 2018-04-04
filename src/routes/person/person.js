a73f8e6bc7a2cb411eda1e51b048c2e061cba084
alisa <alisa@dxt.cn>
2018-04-04 17:15:21 +0800

    个人页面

--- src/routes/person/person.js
+++ src/routes/person/person.js
@@ -1,13 +1,42 @@
 import  React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 import GoHome from '../../compontents/goHome/goHome.js';
 import './person.css'
 import head  from '../../images/person-default.png';
 import {GetQueryString} from '../../utils/commit.js';
 export default class Person extends Component{
+
+ constructor(props){
+   super(props);
+   this.state={
+    head:"",
+    id:"",
+    user_name:"",
+    score:0
+    }
+ }
   componentDidMount(){
     if (GetQueryString("openid")) {
       alert("openid="+GetQueryString("openid"))
+      let openid=GetQueryString("openid")
+       this.setState({
+          openid:openid
+        })
+       axios.get(`http://wx.devtop.top/user/info?openid=${openid}`)
+        .then((res)=>{
+          console.log(res.data)
+
+           this.setState({
+                head:res.data.head_imgUrl,
+                id:res.data.id_out,
+                user_name:res.data.user_name,
+                score:res.data.score
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
     }
 
   }
@@ -16,26 +45,21 @@ export default class Person extends Component{
   <div className="pesron">
     <GoHome />
     <div className="profile container">
-       <div className="img"><img src={head} alt="me" /></div>
+       <div className="img"><img src={this.state.head} alt="me" /></div>
      <div className="detail">
-       <h2><span>读书人</span></h2>
-       <p>您的账号ID:np_JyhiKMRA2Q</p>
+       <h2><span>{this.state.user_name}</span></h2>
+       <p>您的账号ID:{this.state.id}</p>
      </div>
      </div>
      <ul className="pesron-list">
       <li className="recharge">
-        <Link to="/pay" >
+        <Link to={this.state.openid?`/pay?openid=${this.state.openid}`:"/pay"} >
           <div className="left"><i></i>&nbsp;&nbsp;充值</div>
-          <div className="right"><span>余额 0阅点+200红包</span><span className="arrow"></span></div>
+          <div className="right"><span>余额 {this.state.score}阅币</span><span className="arrow"></span></div>
         </Link>
       </li>
-      <li className="hongbao">
-         <Link to="/hongbao">
-          <div className="left"><i></i>&nbsp;&nbsp;我的红包</div><div className="right"><span className="arrow"></span></div>
-         </Link>
-      </li>
       <li className="history">
-        <Link to="/history">
+        <Link to={this.state.openid?`/history?openid=${this.state.openid}`:"/history"}>
         <div className="left"><i></i>&nbsp;&nbsp;阅读历史</div><div className="right"><span className="arrow"></span></div>
         </Link>
       </li>
